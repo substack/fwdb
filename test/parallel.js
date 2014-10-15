cd459a1154ec42d758714860d7cdd1013a279dda
James Halliday <mail@substack.net>
2014-10-16 00:49:45 +0800

    even more parallel tests passing

--- test/parallel.js
+++ test/parallel.js
@@ -5,11 +5,16 @@ var fdb = require('../')(db);
 var docs = [
     { key: 'woo', hash: 'aaa' },
     { key: 'woo', hash: 'bbb', prev: [ 'aaa' ] },
-    { key: 'woo', hash: 'ccc', prev: [ 'bbb' ] }
+    { key: 'woo', hash: 'aaa' },
+    { key: 'woo', hash: 'ddd', prev: [ 'bbb' ] },
+    { key: 'woo', hash: 'ccc', prev: [ 'bbb' ] },
+    { key: 'woo', hash: 'ccc', prev: [ 'bbb' ] },
+    { key: 'woo', hash: 'ddd', prev: [ 'bbb' ] },
+    { key: 'woo', hash: 'bbb', prev: [ 'aaa' ] }
 ];
 
 test('parallel duplicate insertions', function (t) {
-    t.plan(docs.length * 3 + 4*2);
+    t.plan(docs.length * 3 + 5*2);
     
     (function next () {
         if (docs.length === 0) return check();
@@ -37,15 +42,25 @@ test('parallel duplicate insertions', function (t) {
         });
         fdb.links('bbb', function (err, links) {
             t.ifError(err);
-            t.deepEqual(links, [ { key: 'woo', hash: 'ccc' } ]);
+            t.deepEqual(links, [
+                { key: 'woo', hash: 'ccc' },
+                { key: 'woo', hash: 'ddd' }
+            ]);
         });
         fdb.links('ccc', function (err, links) {
             t.ifError(err);
             t.deepEqual(links, []);
         });
+        fdb.links('ddd', function (err, links) {
+            t.ifError(err);
+            t.deepEqual(links, []);
+        });
         fdb.heads('woo', function (err, heads) {
             t.ifError(err);
-            t.deepEqual(heads, [ { hash:  'ccc' } ]);
+            t.deepEqual(heads, [
+                { hash:  'ccc' },
+                { hash:  'ddd' }
+            ]);
         });
     }
 });
